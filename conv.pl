481513d18b87b023be4163f444f6c45b9f4c07b9
Ant <11441088+ahope1@users.noreply.github.com>
2021-12-29 21:55:22 +0000

    Better message string handling. Etc.
    1 February 2021

--- conv.pl
+++ conv.pl
@@ -26,6 +26,10 @@ chomp (my $treasury = <$fh>);
 say "NEW";
 say "AUTO";
 
+say "REM Program must be run on a BBC Micro with a 65(C)02 Co-processor";
+say "LOMEM=&C000";
+say "HIMEM=&F800";
+
 say "ONERROR:ONERROROFF:P.:REPORT:P.'ERL:CLOSE#0:END\n";
 
 say "REM header";
@@ -90,7 +94,27 @@ say"\nREM messages";
 for(my $i = 0; $i <= $messages; $i++)
 {
 	chomp(my $message=<$fh>);
-	say "DATA $message";
+	
+	if (length $message>255)
+	{
+		die "Message too long!:\n$message\n";
+	}
+	
+	if (length $message>230)
+	{
+		$message = substr($message,1);
+
+		my @array = ( $message =~ m/.{1,230}/g );
+		foreach(@array[0..$#array-1])
+		{
+			say "DATA ".'"'."$_".'["';
+		}
+		say "DATA ".'"'."@array[$#array]";
+	}
+	else 
+	{
+		say "DATA $message";
+	}
 }
 
 $blank = <$fh>;
@@ -136,10 +160,10 @@ CLOSE#0:D=OPENOUT("DAT"):V.14
 READ IL,CL,NL,RL,MX,R,TT,ln,LT,ML,TR
 PRINT#D,IL,CL,NL,RL,MX,R,TT,ln,LT,ML,TR
 PRINT'"IL=";IL;",CL=";CL",NL=";NL;",RL=";RL;",MX=";MX;",R=";R","'"TT=";TT;",ln=";ln;",LT=";LT;",ML=";ML;",TR=";TR'
-DIMCA(0,7),NV$(NL,1),IA$(IL),IA(IL),RS$(RL),RM(RL,5),MS$(ML),AA(5),A$(9),A(1,7)
+DIMCA%(0,7),NV$(NL,1),IA$(IL),IA(IL),RS$(RL),RM(RL,5),MS$(ML),AA(5),A$(9),A%(1,7):A$=STRING$(255,"*"):A$=""
 
-P."-- CL -- CA(1,7)"
-FORZ=0TOCL:FORY=0TO7:READ CA(0,Y):NEXTY:X=0:PRINT#D,CA(X,0),CA(X,1),CA(X,2),CA(X,3),CA(X,4),CA(X,5),CA(X,6),CA(X,7):PRINT';CA(X,0);",";CA(X,1);",";CA(X,2);",";CA(X,3);",";CA(X,4);",";CA(X,5);",";CA(X,6);",";CA(X,7):NEXTZ
+P."-- CL -- CA%(1,7)"
+FORZ=0TOCL:FORY=0TO7:READ CA%(0,Y):NEXTY:X=0:PRINT#D,CA%(X,0),CA%(X,1),CA%(X,2),CA%(X,3),CA%(X,4),CA%(X,5),CA%(X,6),CA%(X,7):PRINT';CA%(X,0);",";CA%(X,1);",";CA%(X,2);",";CA%(X,3);",";CA%(X,4);",";CA%(X,5);",";CA%(X,6);",";CA%(X,7):NEXTZ
 READ Z:PRINT#D,Z:P.Z
 
 OS."FX21":V.7,10,13,136:P."PRESS A KEY"':IFGET
@@ -154,29 +178,27 @@ P.;RM(X,0);" ";RM(X,1);" ";RM(X,2);" ";RM(X,3);" ";RM(X,4);" ";RM(X,5)'RS$(X):NE
 OS."FX21":V.7,10,13,136:P."PRESS A KEY"':IFGET
 
 P."-- ML -- MS$(ML)"
-FORX=0TOML:READ MS$(X):P."MS$(";X;")="MS$(X):NEXT
+FORX=0TOML:READ A$
+IF RIGHT$(A$,1)<>"[" MS$(X)=A$ ELSE MS$(X)=STRING$(255,"*"):MS$(X)=LEFT$(A$,(LENA$)-1):REP.READ A$:IF RIGHT$(A$,1)="[" MS$(X)=MS$(X)+LEFT$(A$,(LENA$)-1):U.0 EL.MS$(X)=MS$(X)+A$:U.1
+P."MS$(";X;")="MS$(X):NEXT
 OS."FX21":V.7,10,13,136:P."PRESS A KEY"':IFGET
 
 P."-- IL -- IA$(IL)"
-FORX=0TOIL:READ IA$(X),IA(X):P."IA$(";X;")=";IA$(X)'"IA(";X;")=";IA(X):NEXT
+FORX=0TOIL:READ A$,IA(X):IA$(X)=A$:P."IA$(";X;")=";IA$(X)'"IA(";X;")=";IA(X):NEXT
 OS."FX21":V.7,10,13,136:P."PRESS A KEY"':IFGET
 
 P."Writing data to storage medium..."
 
 FORX=0TONL:FORY=0TO1:PRINT#D,NV$(X,Y)
-REMP."NV$(";X;",";Y;")="NV$(X,Y):
 NEXTY,X
 
 FORX=0TORL:PRINT#D,RM(X,0),RM(X,1),RM(X,2),RM(X,3),RM(X,4),RM(X,5),RS$(X)
-REMPRINT"Room ";X'RM(X,0),RM(X,1),RM(X,2),RM(X,3),RM(X,4),RM(X,5)'">"RS$(X)"<":
 NEXT
 
 FORX=0TOML:PRINT#D,MS$(X)
-REMPRINT"Message ";X;" >"MS$(X)"<":
 NEXT
 
 FORX=0TOIL:PRINT#D,IA$(X),IA(X)
-REMPRINT"Object ";X'">"IA$(X)"<"'IA(X)':
 NEXT
 
 CLOSE#D
@@ -187,26 +209,23 @@ INPUT"REWIND TAPE TO BE VERFIED";TP$
 D=OPENIN("DAT")
 RESTORE
 
-FORX=1TO11:READA0:NEXT
-INPUT#D,A0,A1,A2,A3,A4,A5,A6,A7,A8,A9,B0
-IFB0<>TR OR A0<>IL OR A1<>CL OR A2<>NL OR A3<>RL OR A4<>MX OR A5<>R OR(A6<>TT) OR A7<>ln OR(A8<>LT) OR A9<>ML THEN PROC6790
+FORX=1TO11:READA0%:NEXT
+INPUT#D,A0%,A1,A2,A3,A4,A5,A6,A7,A8,A9,B0
+IFB0<>TR OR A0%<>IL OR A1<>CL OR A2<>NL OR A3<>RL OR A4<>MX OR A5<>R OR(A6<>TT) OR A7<>ln OR(A8<>LT) OR A9<>ML THEN PROC6790
 
-FORX=0TOCL:INPUT#D,A(0,0),A(0,1),A(0,2),A(0,3),A(0,4),A(0,5),A(0,6),A(0,7)
-FORZ=0TO7:READ A0:IFA(0,Z)<>A0,THEN PROC6790 ELSE NEXT Z,X
-READ A0:INPUT#D,Z:IF A0<>Z PROC6790
+FORX=0TOCL:INPUT#D,A%(0,0),A%(0,1),A%(0,2),A%(0,3),A%(0,4),A%(0,5),A%(0,6),A%(0,7)
+FORZ=0TO7:READ A0%:IFA%(0,Z)<>A0%,THEN PROC6790 ELSE NEXT Z,X
+READ A0%:INPUT#D,Z:IF A0%<>Z PROC6790
 
 FORX=0TONL:FORY=0TO1:INPUT#D,A$(0):IFNV$(X,Y)<>A$(0) THEN PROC6790 ELSE NEXTY,X
 
 FORX=0TORL:INPUT#D,AA(0),AA(1),AA(2),AA(3),AA(4),AA(5),A$
-REMP."Room ";X'AA(0),AA(1),AA(2),AA(3),AA(4),AA(5)'">"A$"<":
 FORY=0TO5:IFAA(Y)<>RM(X,Y)ORA$<>RS$(X)THEN PROC6790 ELSE NEXT Y,X
 
 FORX=0TOML:INPUT#D,A$
-REMP."Message ";X'">"A$"<":
 IFA$<>MS$(X)THEN PROC6790 ELSE NEXT
 
 FORX=0TOIL:INPUT#D,A$,AA(0)
-REMP."Object ";X'">"A$"<"'AA(0):
 IFAA(0)<>IA(X)ORA$<>IA$(X)THEN PROC6790 ELSE NEXT
 
 CLOSE#D:PRINT"DATA TAPE VERIFIED"
