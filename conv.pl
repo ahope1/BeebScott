b1138b3c483a106fc9c1238d87763850747cee1c
Ant <11441088+ahope1@users.noreply.github.com>
2021-12-29 22:13:51 +0000

    Move W% flag
    19 February 2021

--- conv.pl
+++ conv.pl
@@ -11,6 +11,7 @@ use IO::File;
 # 2021-02-05 -----  Allow for message that starts with line-break.
 # 2021-02-12 19:43  Fixed nonautoindex. Fixed dates in these comments!
 # 2021-02-19 -----  Detect and record use of manual newlines.
+# 2021-02-19 21:45  Moved W% flag to first DATA line in BASIC prog.
 
 my $fname = $ARGV[0];
 
@@ -57,16 +58,18 @@ say "HIMEM=&F800";
 
 say "ONERROR:ONERROROFF:P.:REPORT:P.'ERL:CLOSE#0:END\n";
 
-say "REM header";
-say "REM IL,CL,NL,RL,MX,R,TT,ln,LT,ML,TR";
+my @basic;
 
-say "DATA $objects,$actions,$words,$rooms,$mx,$start,$treasures,$wordlen,$lt,$messages,$treasury\n";
+push @basic, 'REM header';
+push @basic, 'REM IL,CL,NL,RL,MX,R,TT,ln,LT,ML,TR';
+
+push @basic, "DATA $objects,$actions,$words,$rooms,$mx,$start,$treasures,$wordlen,$lt,$messages,$treasury\n";
 
 my $blank = <$fh>;
 
 ########################################
 
-say "REM actions";
+push @basic, 'REM actions';
 
 my $nonautoindex = 0;
 my $found = 0;
@@ -75,7 +78,7 @@ for(my $i = 0; $i <= $actions; $i++)
 {
 	chomp(my $action=<$fh>);
 	$action =~ tr/ /,/;
-	say "DATA $action";
+	push @basic, "DATA $action";
 	
 	if ($found==0 && $action =~ /([0-9]+),/)
 	{
@@ -87,13 +90,13 @@ for(my $i = 0; $i <= $actions; $i++)
 	}
 }
 
-say "DATA $nonautoindex";
+push @basic, "DATA $nonautoindex";
 
 $blank = <$fh>; 
 
 ########################################
 
-say"\nREM vocab";
+push @basic, "\nREM vocab";
 for(my $i = 0; $i <= $words; $i++)
 {
 	chomp(my $wordpair=<$fh>);
@@ -106,14 +109,14 @@ for(my $i = 0; $i <= $words; $i++)
 	if ($i==4){ $dirn = substr("WEST",0,$wordlen); $wordpair =~ s/,"$dirn"/,"WEST"/; }
 	if ($i==6){ $dirn = substr("DOWN",0,$wordlen); $wordpair =~ s/,"$dirn"/,"DOWN"/; }
 	
-	say "DATA $wordpair";
+	push @basic, "DATA $wordpair";
 }
 
 $blank = <$fh>;
 
 ########################################
 
-say"\nREM rooms";
+push @basic, "\nREM rooms";
 for(my $i = 0; $i <= $rooms; $i++)
 {
 	chomp(my $room=<$fh>);
@@ -131,7 +134,7 @@ for(my $i = 0; $i <= $rooms; $i++)
 
 	$room =~ tr/`/'/;	
 	
-	say "DATA $room";
+	push @basic, "DATA $room";
 }
 
 $blank = <$fh>;
@@ -141,7 +144,7 @@ $blank = <$fh>;
 # manual newline flag
 my $w = 0;
 
-say"\nREM messages";
+push @basic, "\nREM messages";
 for(my $i = 0; $i <= $messages; $i++)
 {
 	chomp(my $message=<$fh>);
@@ -173,17 +176,19 @@ for(my $i = 0; $i <= $messages; $i++)
 		my @array = ( $message =~ m/.{1,230}/g );
 		foreach(@array[0..$#array-1])
 		{
-			say "DATA ".'"'."$_".'["';
+			push @basic, "DATA ".'"'."$_".'["';
 		}
-		say "DATA ".'"'."$array[$#array]";
+		push @basic, "DATA ".'"'."$array[$#array]";
 	}
 	else 
 	{
-		say "DATA $message";
+		push @basic, "DATA $message";
 	}
 }
 
-say "DATA $w";
+say "REM manual newline flag\nDATA $w\n";
+
+say for @basic;
 
 $blank = <$fh>;
 
@@ -228,6 +233,8 @@ CLS:INPUT"PREPARE DATA TAPE (HIT ENTER)";TP$:D=-1
 CLS:PRINT"ADVENTURE DATA TAPE BUILDER - WORKING"
 CLOSE#0:D=OPENOUT("DAT"):V.14
 
+READ W%:PRINT#D,W%
+
 READ IL,CL,NL,RL,MX,R,TT,ln,LT,ML,TR
 PRINT#D,IL,CL,NL,RL,MX,R,TT,ln,LT,ML,TR
 PRINT'"IL=";IL;",CL=";CL",NL=";NL;",RL=";RL;",MX=";MX;",R=";R","'"TT=";TT;",ln=";ln;",LT=";LT;",ML=";ML;",TR=";TR'
@@ -252,7 +259,6 @@ P."-- ML -- MS$(ML)"
 FORX=0TOML:READ A$
 IF RIGHT$(A$,1)<>"[" MS$(X)=A$ ELSE MS$(X)=STRING$(255,"*"):MS$(X)=LEFT$(A$,(LENA$)-1):REP.READ A$:IF RIGHT$(A$,1)="[" MS$(X)=MS$(X)+LEFT$(A$,(LENA$)-1):U.0 EL.MS$(X)=MS$(X)+A$:U.1
 P."MS$(";X;")="MS$(X):NEXT
-READ W%
 OS."FX21":V.7,10,13,136:P."PRESS A KEY"':IFGET
 
 P."-- IL -- IA$(IL)"
@@ -269,7 +275,6 @@ NEXT
 
 FORX=0TOML:PRINT#D,MS$(X)
 NEXT
-PRINT#D,W%
 
 FORX=0TOIL:PRINT#D,IA$(X),IA(X)
 NEXT
@@ -282,33 +287,30 @@ INPUT"REWIND TAPE TO BE VERFIED";TP$
 D=OPENIN("DAT")
 RESTORE
 
+READ W%:INPUT#D,Z:IF W%<>Z STOP
+
 FORX=1TO11:READA0%:NEXT
 INPUT#D,A0%,A1,A2,A3,A4,A5,A6,A7,A8,A9,B0
-IFB0<>TR OR A0%<>IL OR A1<>CL OR A2<>NL OR A3<>RL OR A4<>MX OR A5<>R OR(A6<>TT) OR A7<>ln OR(A8<>LT) OR A9<>ML THEN PROC6790
+IFB0<>TR OR A0%<>IL OR A1<>CL OR A2<>NL OR A3<>RL OR A4<>MX OR A5<>R OR(A6<>TT) OR A7<>ln OR(A8<>LT) OR A9<>ML THEN STOP
 
 FORX=0TOCL:INPUT#D,A%(0,0),A%(0,1),A%(0,2),A%(0,3),A%(0,4),A%(0,5),A%(0,6),A%(0,7)
-FORZ=0TO7:READ A0%:IFA%(0,Z)<>A0%,THEN PROC6790 ELSE NEXT Z,X
-READ A0%:INPUT#D,Z:IF A0%<>Z PROC6790
+FORZ=0TO7:READ A0%:IFA%(0,Z)<>A0% THEN STOP ELSE NEXT Z,X
+READ A0%:INPUT#D,Z:IF A0%<>Z STOP
 
-FORX=0TONL:FORY=0TO1:INPUT#D,A$(0):IFNV$(X,Y)<>A$(0) THEN PROC6790 ELSE NEXTY,X
+FORX=0TONL:FORY=0TO1:INPUT#D,A$(0):IFNV$(X,Y)<>A$(0) THEN STOP ELSE NEXTY,X
 
 FORX=0TORL:INPUT#D,AA(0),AA(1),AA(2),AA(3),AA(4),AA(5),A$
-FORY=0TO5:IFAA(Y)<>RM(X,Y)ORA$<>RS$(X)THEN PROC6790 ELSE NEXT Y,X
+FORY=0TO5:IFAA(Y)<>RM(X,Y)ORA$<>RS$(X)THEN STOP ELSE NEXT Y,X
 
 FORX=0TOML:INPUT#D,A$
-IFA$<>MS$(X)THEN PROC6790 ELSE NEXT
-INPUT#D,Z:IF W%<>Z PROC6790
+IFA$<>MS$(X)THEN STOP ELSE NEXT
 
 FORX=0TOIL:INPUT#D,A$,AA(0)
-IFAA(0)<>IA(X)ORA$<>IA$(X)THEN PROC6790 ELSE NEXT
+IFAA(0)<>IA(X)ORA$<>IA$(X)THEN STOP ELSE NEXT
 
 CLOSE#D:PRINT"DATA TAPE VERIFIED"
 
 END
-
-DEFPROC6790
-PRINT"BAD TAPE":CLOSE#D:END
-ENDPROC
 
 END_MSG
 
